dc6f801a8766f4851d68e020cc0448da867c9b0f
Anthony Martinez <armmartinez158@gmail.com>
2021-09-29 14:40:35 -0500

    This is a SCP Feature

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,7 +3,8 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		System.out.println("This is my first statement.");
-		System.out.println("THIS IS A SCP FEATURE");
+		System.out.println("THIS IS A FEATURE");
+		System.out.println("SCP, don't look");
 
 	}
 
